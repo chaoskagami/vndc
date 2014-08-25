0f3efb8103769cd3bf19ffefbca226f3d02151db
chaoskagami <chaos.kagami@gmail.com>
2014-08-24 21:01:27 -0400

    Implemented voice-stopping on quoted lines.

--- vndc/src/op_sound.cpp
+++ vndc/src/op_sound.cpp
@@ -18,27 +18,40 @@ void op_sound(char* file, int* times) {
 		GetData()->ctx->Audio()->FlushSfx();
 		return;
 	}
+	// Extension - Voiced auto-stop. If the next line contains quotes,
+	// it is voiced. We set a flag in response. If the flag is already
+	// set - e.g. the sound was a voice as well, we stop the previous
+	// sound before playing this one.
+
+	if(GetData()->vndc_enabled &&
+	   (GetData()->next_line[6] == '"' ||
+	    GetData()->next_line[strlen(GetData()->next_line) - 1] == '"')) {
+		if(GetData()->is_spoken_line == true) {
+			GetData()->ctx->Audio()->FlushSfx();
+		}
+		GetData()->is_spoken_line = true;
+	}
+	else
+		GetData()->is_spoken_line = false;
+
+
 	// Play command
+	int count = 1;
+	if (times != NULL)
+		count = *times;
+
+	char path[400];
+	memset(path, 0, 400);
+
+	snprintf(path, 400, "sound/%s", file);
+
+	// Load sfx.
+	int sfxi = GetData()->ctx->Audio()->LoadSfx(path);
+
+	if (count == -1) {
+		GetData()->ctx->Audio()->PlaySfx(sfxi, count);
+	}
 	else {
-		int count = 1;
-		if (times != NULL)
-			count = *times;
-
-		char path[400];
-		memset(path, 0, 400);
-
-		snprintf(path, 400, "sound/%s", file);
-
-		//printf("[op_sound] path: '%s'\n", path);
-
-		// Load displayable.
-		int sfxi = GetData()->ctx->Audio()->LoadSfx(path);
-
-		if (count == -1) {
-			GetData()->ctx->Audio()->PlaySfx(sfxi, count);
-		}
-		else {
-			GetData()->ctx->Audio()->PlaySfx(sfxi, count-1);
-		}
+		GetData()->ctx->Audio()->PlaySfx(sfxi, count-1);
 	}
 }
